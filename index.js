957a3223e18b9ca779fce3650dbfd3e250745943
mitHSwas <wdev.mithunbiswas@gmail.com>
2022-10-22 21:58:18 +0600

    Gorom kobor database and api is copleted

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
 const express = require("express");
 const app = express();
-
+const cors = require("cors");
 const port = 5000;
 
 const category = require("./data/categories.json");
+const news = require("./data/news.json");
+
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.send("Api is creating....")
 })
 
-app.get("/category", (req, res) => {
+app.get("/categories", (req, res) => {
     res.send(category);
+})
+
+app.get("/category/:id", (req, res) => {
+    const id = req.params.id;
+    if (id === '08') {
+        res.send(news);
+    }
+    else {
+        const categoriesNews = news.filter(ns => ns.category_id === id);
+        res.send(categoriesNews)
+    }
+})
+
+app.get("/news", (req, res) => {
+    res.send(news);
+})
+
+app.get("/news/:id", (req, res) => {
+    const id = req.params.id;
+    const newsDetails = news.find(news => news._id === id);
+    res.send(newsDetails);
 })
 
 app.listen(port, () => {
